7ccd68c35a85a749094194c5050901c8f918e3be
Ken Wong <wkmmky@gmail.com>
2016-10-31 02:16:01 +0800

    Testing

---
+++ Simple/Simple.h
@@ -0,0 +1,19 @@
+//
+//  Simple.h
+//  Simple
+//
+//  Created by Ken on 31/10/2016.
+//  Copyright © 2016 kw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Simple.
+FOUNDATION_EXPORT double SimpleVersionNumber;
+
+//! Project version string for Simple.
+FOUNDATION_EXPORT const unsigned char SimpleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Simple/PublicHeader.h>
+
+
---
+++ Simple/TestCon.swift
@@ -0,0 +1,10 @@
+//
+//  TestCon.swift
+//  Simple
+//
+//  Created by Ken on 31/10/2016.
+//  Copyright © 2016 kw. All rights reserved.
+//
+
+import Foundation
+import RxSwift
